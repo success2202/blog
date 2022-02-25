bf338cdb9537fd237d060654a0de0ae1c69d6333
unknown <successceejay@gmail.com>
2022-02-25 13:26:12 +0000

    added new update

---
+++ refresh/loginfunc.php
@@ -0,0 +1,25 @@
+
+
+<?php
+
+ $_POST["username"]; 
+
+ $_POST["password"];
+$_POST["country"];
+$country = $_POST["country"];
+
+$country = $country;
+switch ($country){
+    case 'Nig':
+        echo "you are from Nigeria";
+        break;
+        case 'Us':
+            echo "you are from the united state of america";
+            break;
+            case 'Can':
+                echo "you are from Canada";
+                break;
+}
+
+?>
+
