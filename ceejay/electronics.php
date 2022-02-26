1b2d4bf8363b9a3872ca52f5a4ceced29eeeb983
unknown <successceejay@gmail.com>
2022-02-26 23:23:14 +0000

    added electronics

---
+++ ceejay/electronics.php
@@ -0,0 +1,39 @@
+<?php
+class electronics{
+	public $name;
+	public $color;
+	public $use;
+	public $price;
+	function ElectronName($name){
+		return $this->name = $name;
+	}
+
+	function ElectronColor($color){
+		return $this->color = $color;
+	}
+
+	function ElectronUse($use){
+		return $this->use = $use;
+	}
+
+	function ElectronPrice($price){
+		return $this->price = $price;
+	}
+}
+
+$television = new electronics();
+
+$e_name = $television ->ElectronName('the name of the electronics is LG television');
+$e_color = $television ->ElectronColor('the color is red');
+$e_use = $television ->ElectronUse('it is use for watching movies');
+$e_price = $television ->ElectronPrice('the current price is 200000 thousand naira');
+echo "this is the television details";
+echo "<br>";
+echo "$e_name";
+echo "<br>";
+echo "$e_color";
+echo "<br>";
+echo "$e_use";
+echo "<br>";
+echo "$e_price";
+?>
