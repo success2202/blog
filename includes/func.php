ae7a2d5830b1e2017368d2ee23f78ff13fd07a25
Michael Kachi <kachi@Michaels-MBP.lan>
2022-03-06 23:16:59 +0100

    functions added

--- includes/func.php
+++ includes/func.php
@@ -1,28 +1,41 @@
 <?php
 include('core/db_connect.php');
 
+function get_all_subjects(){
+  global $con;
+  $subj = "SELECT * FROM subjects ORDER BY position ASC";
+  $chk = mysqli_query($con, $subj);
+  return $chk;
+}
+
+function get_pages_for_subject($id){
+  global $con;
+  $page = "SELECT * FROM pages WHERE subject_id = $id";
+      $chks = mysqli_query($con, $page);
+    return $chks;
+}
+
 function navigation(){
         global $con;
-        $subj = "SELECT * FROM subjects ORDER BY position ASC";
-        $chk = mysqli_query($con, $subj);
+       
         $output =  "<ul class=\"subjects\">";
+        $chk = get_all_subjects();
         while($subj_set = mysqli_fetch_assoc($chk)){
             $id = $subj_set['id'];
           $output .= "<li>".$subj_set['menu_name'];
           $output .= "<ul class=\"pages\">";
-          $page = "SELECT * FROM pages WHERE subject_id = $id";
-          $chks = mysqli_query($con, $page);
+          $chks = get_pages_for_subject($id);
             while($pages = mysqli_fetch_assoc($chks)){
                 $output .= "<li>". $pages['menu_name']."</li>";
             }
           $output .="</ul>";
            $output .= "</li>";
-          
-
 }
 $output .= "</ul>";
          return $output;
 }
 
+
+    
 
 ?>
