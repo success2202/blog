3bc73285957664704b82c87e3a822a4dd606bdca
unknown <successceejay@gmail.com>
2022-03-11 12:22:28 +0000

    login form

---
+++ form/register.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>user registration</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+
+
+<div class="login-box">
+<div class="col-md=6 login-right">
+				<h2>Register Here</h2>
+				<form action="validate.php" method="post">
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" name="user" class="form-control" required>
+					</div>
+                    <div class="form-group">
+						<label>Email</label>
+						<input type="email" name="user" class="form-control" required>
+					</div>
+                    <div class="form-group">
+						<label>Country</label>
+						<input type="text" name="user" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="password" class="form-control" required>
+					</div>
+<button type="submit" class="btn btn-primary">Register</button>
+				</form>
+			</div>
+            </div>
+
+            </body>
+</html>
